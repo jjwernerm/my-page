750952519ded85b56391113d8df86316992e4bdb
jjwernerm <joannywerner@gmail.com>
2023-06-02 16:34:29 -0400

    Creación del componente Navbar

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './views/Home';
+import About from './views/About';
+import Portfolio from './views/Portfolio';
+import Contact from './views/Contact';
+import Blog from './views/Blog';
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App;
