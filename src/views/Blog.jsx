6c42817222f256f7cabc35bf5b3115f56c481218
jjwernerm <joannywerner@gmail.com>
2023-06-22 01:51:16 -0400

    Se actualiza el componente Contact con el envío del formulario y el servicio de Formspree

--- src/views/Blog.jsx
+++ src/views/Blog.jsx
@@ -2,10 +2,13 @@ function Blog() {
 
   return (
     <>
-      <div className="min-h-views text-white tracking-widest grid grid-cols-1 items-center justify-items-center">
-        <div className="col-1 text-xl text-center">
-          <p className="tracking-widest">Blog</p><br />
-          <p className="tracking-tighter">Servicio en mantenimiento...</p>        
+      <div className="bg-color pt-8">
+
+        <div className="min-h-views min-h-screen text-white tracking-widest grid grid-cols-1 items-center justify-items-center">
+          <div className="col-1 text-xl text-center">
+            <p className="tracking-widest">Blog</p><br />
+            <p className="tracking-tighter">Servicio en mantenimiento...</p>        
+          </div>
         </div>
       </div>
     </>
--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -12,8 +12,13 @@ export default function Contact() {
   //Declaro el estado 'warning' utilizando el hook useSate, el estado lo inicializo con un array vacío ([]) y proporciono una función para actualizar el estado: 'setWarning'
   const [warning, setWarning] = useState([]);
 
+    //Declaro los estado 'messageOk', 'messageError' y 'messageEmail' para el manejo de errores en el envío del formulario
+  const [messageOk, setMessageOk] = useState("");
+  const [messageError, setMessageError] = useState("");
+  const [messageEmail, setMessageEmail] = useState("");
+
   //Defino la dunción handleSubmit que se ejecutará cuando se envíe el formulario, esta función toma un evento como argumento y llamo a event.preventDefault() para evitar que el formulario se borre cuando la página se recargue
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
     let warnings = [];
@@ -34,10 +39,37 @@ export default function Contact() {
     setWarning(warnings);//Actualizo el estado warning con el array warnings
 
     if (warnings.length === 0) { //Verifico que el array sea igual a cero para enviar el mensaje de envío y borrar el formulario
-      warnings.message = "Gracias por escribirme, pronto nos conoceremos.";
-      setName ('');
-      setEmail ('');
-      setComment ('');
+      try {
+        const response = await fetch("https://formspree.io/f/mgedqndg", {//Aquí utilizo la función fetch para enviar una solicitud POST al servicio de Formspree. ingreso la URL de acción del formulario como el primer argumento de fetch
+          method: "POST",//Establezco el método de solicitud como "POST" y especifico el encabezado "Content-Type": "application/json". Esto indica que los datos se enviarán en formato JSON.
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({//En el cuerpo de la solicitud (body), utilizo JSON.stringify para convertir un objeto JavaScript que contiene los campos 'name', 'email' y 'comment' en una cadena JSON.
+            name,
+            email,
+            comment,
+          }),
+        });
+
+        //Utilizo un bloque try-catch para manejar posibles errores durante el envío del formulario. Si se produce un error en el bloque try, se captura en el bloque catch y se ejecutan las instrucciones correspondientes.
+
+        //Dentro del bloque try, verifico if (response.ok) para comprobar si la respuesta del servidor fue exitosa. Si es así, significa que el formulario se envió correctamente y se ejecutan las siguientes instrucciones:
+        if (response.ok) {
+          setMessageOk("Gracias por escribirme, pronto nos conoceremos.");
+          setName("");
+          setEmail("");
+          setComment("");
+        } else { //significa que ocurrió un error en el envío del formulario y se ejecutan las siguientes instrucciones:
+          // Opcional: Manejar errores en el envío del formulario
+          setMessageError("Ha ocurrido un error. Por favor, inténtalo de nuevo más tarde.");
+          setMessageEmail("Si el error cntinúa escribe un correo a joannywerner@gmail.com");
+        }
+      } catch (error) {
+        // Opcional: Manejar errores en el envío del formulario
+        setMessageError("Ha ocurrido un error. Por favor, inténtalo de nuevo más tarde.");
+        setMessageEmail("Si el error cntinúa escribe un correo a joannywerner@gmail.com");
+      }
     }
   };
 
@@ -56,8 +88,11 @@ export default function Contact() {
           <div className="flex justify-center mt-5">
             <div className="bg-slate-700 w-4/5 rounded-lg">          
               <form
-              onSubmit={handleSubmit}
-              className="p-10">
+                onSubmit={handleSubmit}
+                action="https://formspree.io/f/mgedqndg"
+                method="POST"
+                className="p-10"
+              >
 
                 <div className="lg:columns-2">
                   <div className="mb-10">
@@ -137,15 +172,18 @@ export default function Contact() {
                   <input
                   type="submit"
                   value="Enviar"
-                  className="text-white bg-cyan-600 border-cyan-600 border-2 rounded p-2 hover:bg-slate-700 transition duration-700 cursor-pointer"
+                  className="text-white bg-cyan-600 border-cyan-600 w-full lg:w-3/12 border-2 rounded p-2 hover:bg-slate-700 transition duration-700 cursor-pointer"
                   />
                 </div>
                 <div>
-                  {warning.message && (<p className="bg-gray-300 text-green-600 font-bold text-center mt-5 p-2 rounded">{warning.message}</p>)}
+                  {messageOk && (<p className="bg-gray-300 text-green-600 font-bold text-center mt-5 p-2 rounded">{messageOk}</p>)}
+                  {messageError && (<p className="text-red-500 text-center mt-5 rounded">{messageError}</p>)}
+                  {messageEmail && (<p className="text-gray-300 text-center rounded">{messageEmail}</p>)}
                   {/* 
                       Mediante esta expresión de JS:
-                      1. accedo a la propiedad 'message' del objeto warning
-                      2. utilizo el operador lógico && para realizar una evaluación cortocicuitada, si la expresión de la izquierda es true se renderiza el elemento <p> por lo que devuelve el resultado de la derecha, en caso contrario, se detiene la evaluación
+                      1. accedo a la propiedad 'messageOk', 'messageError' y 'messageEmail'
+                      2. utilizo el operador lógico && para realizar una evaluación cortocicuitada, si la expresión de la izquierda es true, devuelve el resultado de la derecha, en caso contrario, se detiene la evaluación
+                      Información: Esto es a través de los manejadores de errores 'catch' en caso de que ocurra algún problema durante el envío del formulario.
                     */}
                 </div>
 
