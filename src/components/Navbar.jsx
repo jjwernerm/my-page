c358a390c3608b82ad1b3088f120f6a1de473569
jjwernerm <joannywerner@gmail.com>
2024-07-09 23:10:38 -0400

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -106,7 +106,7 @@ export default function Navbar() {
                       <Menu as="div" className="relative ml-3">
                         <div>
                           <Menu.Button className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                            <span className="sr-only">Open user menu</span>
+                            <span className="sr-only">Abrir menú del perfil</span>
                             <img className="h-8 w-8 rounded-full" src={user.imageUrl} alt="My Photo" />
                           </Menu.Button>
                         </div>
